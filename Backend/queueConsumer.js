3592a4ee48b4cf8ec4dbd5a3e009eb166220c23f
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2025-04-28 14:34:13 +0530

    Nodemon to node

--- Backend/queueConsumer.js
+++ Backend/queueConsumer.js
@@ -1,7 +1,6 @@
 // Load environment variables
 require('dotenv').config();
 
-
 // Import required modules
 const { getBidFromQueue } = require('./utils/sqs/consumer');
 const express = require('express');
@@ -26,26 +25,29 @@ io.on("connection", (socket) => {
   });
 });
 
-setInterval(async () => {
-  try {
-    await connectDB();
-    const result = await getBidFromQueue();
-    if (result && result.Messages && result.Messages.length > 0) {
-      const message = JSON.parse(result.Messages[0].Body);
-      
-      // Emit new bid event to user's specific room
-      io.to(message.user._id).emit('newBid', {
-        userId: message.user._id,
-        message: 'Bid placed successfully!',
-        bid: message
-      });
-      
-    }
-  } catch (error) {
-    console.error('Error processing message:', error);
-  }
-}, 3000);
+(async () => {
+  await connectDB();
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  setInterval(async () => {
+    try {
+      const result = await getBidFromQueue();
+      if (result && result.Messages && result.Messages.length > 0) {
+        const message = JSON.parse(result.Messages[0].Body);
+        io.to(message.user._id).emit('newBid', {
+          userId: message.user._id,
+          message: 'Bid placed successfully!',
+          bid: message
+        });
+      }
+    } catch (error) {
+      console.error('Error processing message:', error);
+    }
+  }, 3000);
+})();
+
+
+// IMPORTANT: Dynamic port for hosting
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Queue Consumer Server running on port ${PORT}`);
 });
