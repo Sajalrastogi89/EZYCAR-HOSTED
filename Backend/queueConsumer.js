757d58b26ab72eaea9297aaf5fe605d1e596fb61
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2025-04-29 13:53:53 +0530

    queueConsumer

--- Backend/queueConsumer.js
+++ Backend/queueConsumer.js
@@ -46,9 +46,7 @@ io.on("connection", (socket) => {
 })();
 
 
-// IMPORTANT: Dynamic port for hosting
-// const PORT = process.env.PORT || 3000;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Queue Consumer Server running on port ${PORT}`);
 });
